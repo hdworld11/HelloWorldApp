3500799b4d6925d23abfdb379487b0aee52d81b3
Harshita <harshita.d11@gmail.com>
2023-06-27 06:11:26 -0400

    Initial Commit

---
+++ HelloWorldApp/HelloWorldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppApp.swift
+//  HelloWorldApp
+//
+//  Created by Daddala, Harshita on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
